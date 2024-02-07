547940d39f1016c0d1a9aa42ddde16e61f717129
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-02-07 10:46:06 +0800

    使用scoped-preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const { scopedPreflightStyles } = require("tailwindcss-scoped-preflight");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
+  plugins: [
+    // ... other plugins
+    scopedPreflightStyles({
+      cssSelector: ".qzq-playlist-container", // or .tailwind-preflight or even [data-twp=true] - any valid CSS selector of your choice
+      mode: "matched only", // it's the default
+    }),
+  ],
+  // corePlugins: {
+  //   preflight: false,
+  // },
   important: ".qzq-playlist-container",
 };
